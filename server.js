d5cce8f3f66f960b00f53753fcb5e2a895570b73
hiuhuedd <hiuhuedd@gmail.com>
2026-05-28 13:09:11 +0300

    mpesa client payout2

--- server.js
+++ server.js
@@ -50,18 +50,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-const allowedOrigins = [
-  '*'
-];
+
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   credentials: true
 }));
 
